bec407bb3207c563b3c6d2f0fb88e6b3548b23f3
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2025-03-31 03:19:31 +0330

    change app icon

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,10 @@
-import AppLogoIcon from './app-logo-icon';
+import { ClipboardPenLine } from 'lucide-react';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <ClipboardPenLine className="size-5 text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">{appName}</span>
